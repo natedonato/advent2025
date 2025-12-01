ab4cba7ba8f97ffa60f3b7fb94c234f91009c72a
natedonato <natedonato@gmail.com>
2025-11-30 22:03:55 -0800

    1

---
+++ 1/part2.py
@@ -0,0 +1,22 @@
+with open("./input.txt", "r") as file:
+    input = file.read().split("\n")[:-1]
+
+start = 50
+count = 0
+
+for line in input:
+    dir = line[0]
+    num = int(line[1:])
+    sign = 1
+
+    if dir == "L":
+        sign = -1
+
+    for _ in range(num):
+        start += sign
+        start %= 100
+
+        if start == 0:
+            count += 1
+
+print(count)
